5f98103d04c45d33936d8cf137f968c6edbbf4e9
baozhangchi <baozhangchi@live.com>
2023-08-15 17:57:00 +0800

    添加扩展

---
+++ src/Packages.Windows/Extensions/StringExtensions.cs
@@ -0,0 +1,40 @@
+﻿#region
+
+using System.Globalization;
+using System.Windows;
+using System.Windows.Media;
+
+#endregion
+
+namespace System;
+
+/// <summary>
+/// 字符串扩展方法
+/// </summary>
+public static class StringExtensions
+{
+    #region Methods
+
+    /// <summary>
+    /// 测量字符串
+    /// </summary>
+    /// <param name="input"></param>
+    /// <param name="container"></param>
+    /// <param name="fontFamily"></param>
+    /// <param name="fontStyle"></param>
+    /// <param name="fontWeight"></param>
+    /// <param name="fontStretch"></param>
+    /// <param name="fontSize"></param>
+    /// <returns></returns>
+    public static Size MeasureString(this string input, Visual container, FontFamily fontFamily, FontStyle fontStyle, FontWeight fontWeight, FontStretch fontStretch, double fontSize)
+    {
+        var formattedText = new FormattedText
+        (
+            input, CultureInfo.CurrentUICulture, FlowDirection.LeftToRight, new Typeface(fontFamily, fontStyle, fontWeight, fontStretch), fontSize, Brushes.Black,
+            VisualTreeHelper.GetDpi(container).PixelsPerDip
+        );
+        return new Size(formattedText.Width, formattedText.Height);
+    }
+
+    #endregion
+}
---
+++ src/Packages.Windows/MarkupExtensions/EnumBindingSourceExtension.cs
@@ -0,0 +1,70 @@
+﻿#region
+
+using System.Windows.Markup;
+
+#endregion
+
+namespace Packages.Windows.MarkupExtensions;
+
+/// <inheritdoc />
+public class EnumBindingSourceExtension : MarkupExtension
+{
+    #region Fields
+
+    private Type? _enumType;
+
+    #endregion
+
+    #region Properties
+
+    /// <summary>
+    ///     枚举类型
+    /// </summary>
+    /// <exception cref="ArgumentException"></exception>
+    public Type? EnumType
+    {
+        get => _enumType;
+        set
+        {
+            if (_enumType != value)
+            {
+                if (value != null)
+                {
+                    var enumType = Nullable.GetUnderlyingType(value) ?? value;
+                    if (!enumType.IsEnum)
+                    {
+                        throw new ArgumentException("必须是一个枚举类型");
+                    }
+                }
+
+                _enumType = value;
+            }
+        }
+    }
+
+    #endregion
+
+    #region Methods
+
+    /// <inheritdoc />
+    public override object ProvideValue(IServiceProvider serviceProvider)
+    {
+        if (_enumType == null)
+        {
+            throw new InvalidOperationException("必须先指定EnumType");
+        }
+
+        var enumType = Nullable.GetUnderlyingType(_enumType) ?? _enumType;
+        var enumValues= Enum.GetValues(enumType);
+        if (enumType == _enumType)
+        {
+            return enumValues;
+        }
+
+        var tempArray = Array.CreateInstance(enumType, enumValues.Length + 1);
+        enumValues.CopyTo(tempArray,1);
+        return tempArray;
+    }
+
+    #endregion
+}
