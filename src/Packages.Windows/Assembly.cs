2242bbe78046302a9d3b09eebbd89d4d1e69a5b1
baozhangchi <baozhangchi@live.com>
2023-08-22 13:35:54 +0800

    添加CommandParametersToTupleConverter

--- src/Packages.Windows/Assembly.cs
+++ src/Packages.Windows/Assembly.cs
@@ -5,5 +5,6 @@ using System.Windows.Markup;
 #endregion
 
 [assembly: XmlnsDefinition("https://github.com/baozhangchi/dotnetpackages", "Packages.Windows.MarkupExtensions")]
+[assembly: XmlnsDefinition("https://github.com/baozhangchi/dotnetpackages", "Packages.Windows.Converters")]
 [assembly: XmlnsDefinition("https://github.com/baozhangchi/dotnetpackages", "Packages.Windows.Behaviors")]
 [assembly: XmlnsPrefix("https://github.com/baozhangchi/dotnetpackages", "dp")]
---
+++ src/Packages.Windows/Converters/CommandParametersToTupleConverter.cs
@@ -0,0 +1,49 @@
+﻿#region
+
+using System;
+using System.Globalization;
+using System.Linq;
+using System.Reflection;
+using System.Windows.Data;
+
+#endregion
+
+namespace Packages.Windows.Converters
+{
+    /// <inheritdoc />
+    public class CommandParametersToTupleConverter : IMultiValueConverter
+    {
+        private static CommandParametersToTupleConverter _instance;
+
+        /// <summary>
+        ///     默认实例
+        /// </summary>
+        public static CommandParametersToTupleConverter Instance =>
+            _instance ?? (_instance = new CommandParametersToTupleConverter());
+
+        /// <inheritdoc />
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (values.Length > 0)
+            {
+                var methods = typeof(Tuple).GetMethods(BindingFlags.Public | BindingFlags.Static)
+                    .Where(x => x.Name == "Create").ToList();
+                var method = methods.FirstOrDefault(x => x.GetParameters().Length == values.Length);
+                if (method != null)
+                {
+                    return method.MakeGenericMethod(values.Select(x => x.GetType()).ToArray()).Invoke(null, values);
+                }
+
+                return null;
+            }
+
+            return null;
+        }
+
+        /// <inheritdoc />
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
