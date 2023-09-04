080f8c20383c1a48a9d75b2b553cb1f708b92e1c
baozhangchi <baozhangchi@live.com>
2023-09-04 14:17:28 +0800

    添加Packages.Avalonia

---
+++ src/Packages.Avaloia/Extensions/AppBuilderExtensions.cs
@@ -0,0 +1,39 @@
+﻿#region
+
+using System.Linq;
+using Avalonia.Media;
+
+#endregion
+
+// ReSharper disable once IdentifierTypo
+// ReSharper disable once CheckNamespace
+namespace Avalonia
+{
+    /// <summary>
+    ///     AppBuilder的扩展方法
+    /// </summary>
+    public static class AppBuilderExtensions
+    {
+        /// <summary>
+        ///     设置默认字体
+        /// </summary>
+        /// <param name="appBuilder"></param>
+        /// <param name="defaultFamilyName">默认字体名称</param>
+        /// <param name="fallbackFonts">备用字体</param>
+        /// <returns></returns>
+        public static AppBuilder UseCustomDefaultFont(this AppBuilder appBuilder, string defaultFamilyName,
+            params string[] fallbackFonts)
+        {
+            var fallbacks = fallbackFonts.ToList();
+            fallbacks.Insert(0, defaultFamilyName);
+            return appBuilder.With(new FontManagerOptions
+            {
+                DefaultFamilyName = defaultFamilyName,
+                FontFallbacks = fallbacks.Select(x => new FontFallback
+                {
+                    FontFamily = new FontFamily(x)
+                }).ToList()
+            });
+        }
+    }
+}
